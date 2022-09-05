0db5de90387be41c008e2d8f580625417e0c9d65
Iugan Flaviu <iflaviu25@gmail.com>
2022-09-05 11:52:29 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-const fsPromises = require("fs").promises;
+const fs = require("fs");
 const util = require("node:util");
 const exec = util.promisify(require("node:child_process").exec);
-
-const readDirectory = () => {
-  const file = fsPromises.readdir("./mp4");
-  return file;
-};
-
-const trimString = async () => {
-  const allSongs = await readDirectory();
-
-  allSongs.forEach(async (file) => {
-    const newFileName = file.replace(/\s/g, "");
-
-    fsPromises.rename(`./mp4/${file}`, `./mp4/${newFileName}`);
-    // const { stdout, stderr } = await exec(`mv ${file} ${newFileName}`, {
-    //   cwd: "./mp4",
-    // });
-    // console.log("stdout:", stdout);
-    // console.error("stderr:", stderr);
-  });
-};
-trimString();
+const readDir = util.promisify(fs.readdir);
 
 const convertMp4ToMp3 = async () => {
-  const allSongs = await readDirectory();
+  const allSongs = await readDir("./mp4");
+
+  allSongs.forEach((file) => {
+    const newFileName = file.replace(/\s/g, "");
+    fs.renameSync(`./mp4/${file}`, `./mp4/${newFileName}`);
+  });
+
   allSongs.forEach(async (file) => {
     let outputName = file.slice(0, -1) + "3";
     const { stdout, stderr } = await exec(
